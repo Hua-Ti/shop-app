b410c912c6e0d0be911e1be2c1b6a3f757bf760b
FSK <2496371772@qq.com>
2023-12-04 16:20:08 +0800

    更改了搜索的部分页面

--- src/views/ProductListPageView.vue
+++ src/views/ProductListPageView.vue
@@ -1,9 +1,16 @@
 <template>
     <div class="shopping-List">
+
+        <div class="shoping-list-top">
+            <span>
+                <van-icon name="arrow-left" />
+            </span>
+            <p>{{ router.currentRoute.value.query.keyWord }}</p>
+        </div>
         <van-tabs v-model:active="active" @click-tab="onClickTab">
             <van-tab :title="item.title" :key="item.sortKey" v-for="item in titleList"></van-tab>
-            
-            <van-tab title="价格">
+
+            <van-tab title="价格" v-if="priceList.length">
                 <van-popup v-model:show="show" round style="width: 100%; height: 50%; padding: 10px;">
                     <div class="price">
                         <span v-for="item in priceList" key="item">{{ item.min }}-{{ item.max }}</span>
@@ -27,10 +34,10 @@
 import { ref, onMounted, reactive } from "vue"
 import { useRouter } from "vue-router";
 import { getKeywordSearch } from "../apic/search"
-import {type sortFilterList,type priceFilterList,type wallDocsList,type leftbottomTagList} from "../typings"
+import { type sortFilterList, type priceFilterList, type wallDocsList, type leftbottomTagList } from "../typings"
 
 const router = useRouter();
-const active = ref(1);
+const active = ref(0);
 const show = ref(false);
 const onClickTab = () => {
     show.value = true;
@@ -55,9 +62,9 @@ onMounted(async () => {
 
     option.q = keyWord.value;
     let data = await getKeywordSearch(option);
-    titleList.value=data.sortFilter;
-    priceList.value=data.priceFilter;
-    wallList.value=data.wall.docs;
+    titleList.value = data.sortFilter;
+    priceList.value = data.priceFilter;
+    wallList.value = data.wall.docs;
     console.log(titleList.value);
     console.log(wallList.value);
     console.log(data.priceFilter);
