4b1e7977a9440fba742a2aa8c100c0686492a8e2
LJY <lijiayan_sun@163.com>
2023-12-08 14:42:05 +0800

    地址修改

--- src/views/AddressManagement.vue
+++ src/views/AddressManagement.vue
@@ -12,7 +12,7 @@
 
 
         <van-address-list v-model="chosenAddressId" :list="list" default-tag-text="默认" @edit="compile($event)"
-            :show-add-button="false">
+            @select="daGou($event)" :show-add-button="false">
         </van-address-list>
 
         <!-- 增加地址 -->
@@ -28,24 +28,27 @@
                         <van-icon name="location-o" size="28" />
                     </div>
 
-                    <!-- <div class="address-bianji2">
+                    <div class="address-bianji2">
                         <van-cell-group inset class="goLocation-fu">
-                            <van-field class="inputclass" v-model="name" label="姓名" label-width="4em" />
+                            <van-field class="inputclass" v-model="name" placeholder="张三" label="姓名" label-width="4em" />
                         </van-cell-group>
 
                         <van-cell-group inset class="goLocation-fu">
-                            <van-field class="inputclass" v-model="tel" type="tel" label="电话" label-width="4em" />
+                            <van-field class="inputclass" placeholder="180xxxxyyyy" v-model="tel" type="tel" label="电话"
+                                label-width="4em" />
                         </van-cell-group>
 
                         <van-cell-group inset class="goLocation-fu">
-                            <van-field class="inputclass" :model-value="diqu" label="地区" label-width="4em" readonly />
+                            <van-field class="inputclass" v-model="diqu" label="地区" label-width="4em"
+                                placeholder="浙江省杭州市西湖区" />
                             <div class="goLocation" @click="goLocation">
                                 <van-icon name="location-o" size="30" />
                             </div>
                         </van-cell-group>
 
                         <van-cell-group inset class="goLocation-fu">
-                            <van-field class="inputclass" v-model="address" type="text" label="详细地址" label-width="4em" />
+                            <van-field class="inputclass" placeholder="景湖街道新华路xxx号" v-model="address" type="text"
+                                label="详细地址" label-width="4em" />
                         </van-cell-group>
 
                         <van-cell class="moren" center title="设为默认收获地址">
@@ -56,11 +59,10 @@
                         </van-cell>
                         <van-button class="moren" type="primary" @click="addSave" block
                             color="linear-gradient(to right, #ff6034, #ee0a24)">保存</van-button>
-
-                    </div> -->
+                    </div>
 
                     <!-- 方法一 -->
-                    <div class="address-bianji">
+                    <!-- <div class="address-bianji">
                         <van-address-edit :area-list="areaList" v-model:show="show" show-set-default
                             :search-result="searchResult" class="van-cell--clickable"
                             :area-columns-placeholder="['请选择', '请选择', '请选择']" @save="onSave" @delete="onDelete"
@@ -71,7 +73,7 @@
                                 isDefault: info!.isDefault,
                             }">
                         </van-address-edit>
-                    </div>
+                    </div> -->
 
                 </van-popup>
             </div>
@@ -87,8 +89,9 @@ import { listProps, showToast } from 'vant';
 import { areaList } from '@vant/area-data';
 import type { AddressEditInstance } from 'vant';
 const router = useRouter();
-const chosenAddressId = ref('1');
+const chosenAddressId = ref('');
 const show = ref(false);
+
 const changevalue = ref(false)
 const list = ref([])
 const searchResult = ref([]);
@@ -99,9 +102,9 @@ const info = ref<Shuju>()
 //方法二的数据，没有用
 const tel = ref('');
 const name = ref('');
-const diqu = ref('选地区');
+const diqu = ref('');
 const address = ref('');
-const checked = ref(true);
+const checked = ref(false);
 const pattern = /^(?:(?:\+|00)86)?1[3-9]\d{9}$/;
 
 
@@ -109,7 +112,12 @@ const pattern = /^(?:(?:\+|00)86)?1[3-9]\d{9}$/;
 const showPopup = () => {
     info.value = [] as any;
     show.value = true;
-};
+    tel.value = ''
+    name.value = ''
+    diqu.value = ''
+    address.value = ''
+    checked.value = false
+}
 interface Shuju {
     name: string
     tel: string
@@ -125,11 +133,104 @@ onMounted(() => {
 const compile = (list: any) => {
     if (list != '[]') {
         show.value = true;
-        // console.log(chosenAddressId.value)
-        console.log(list)
-        info.value = list;
-    }
 
+        console.log(list)
+        tel.value = list.tel
+        name.value = list.name
+        diqu.value = list.diqu;
+        address.value = list.address;
+        checked.value = list.isDefault
+        // console.log(chosenAddressId.value)
+        // console.log(list)
+        // info.value = list;
+
+    }
+}
+
+//打勾的默认地址
+onMounted(() => {
+    let addressList = localStorage.addressList || `[]`;
+    addressList = JSON.parse(addressList);
+    if (addressList != '[]') {
+        console.log('我是第二个onMounted', addressList)
+        for (let i = 0; i <= addressList.length; i++) {
+            const last = addressList[addressList.length - 1].isDefault;
+            if (last) {
+                chosenAddressId.value = addressList[addressList.length - 1].id
+            }
+        }
+    }
+})
+
+//切换打勾
+const daGou = (item: any) => {
+    console.log(item.id)
+    chosenAddressId.value = item.id
+}
+
+
+const onDelete = (val: any) => {
+    console.log(val)
+    showToast('delete')
+};
+
+const onEdit = (item: any, index: any) => showToast('编辑地址:' + index);
+const goLocation = () => {
+    showToast('跳到定位')
+
+}
+
+//方法二的方法，没有用
+const Changeadd = (value: any) => {
+    console.log(value)
+    changevalue.value = value
+}
+const addSave = () => {
+    if (name.value && tel.value && address.value && diqu.value != '') {
+        let addressList = localStorage.addressList || `[]`;
+        addressList = JSON.parse(addressList);
+
+        addressList.push({
+            id: new Date().getTime(),
+            name: name.value,
+            tel: tel.value,
+            address: diqu.value + address.value,
+            diqu: diqu.value,
+            isDefault: changevalue.value,
+
+        });
+
+        console.log(addressList[addressList.length - 1].isDefault)
+        for (let i = 0; i < addressList.length; i++) {
+            const last = addressList[addressList.length - 1].isDefault;
+
+            if (last) {
+                chosenAddressId.value = addressList[addressList.length - 1].id
+                for (let j = 0; j < addressList.length - 1; j++) {
+                    addressList[j].isDefault = false;
+                }
+            }
+        }
+
+        list.value = addressList
+        localStorage.addressList = JSON.stringify(addressList);
+        // console.log(val)
+        console.log(addressList)
+        showToast({
+            message: '添加成功',
+            icon: 'success',
+        });
+        name.value = ''
+        tel.value = ''
+        address.value = ''
+        diqu.value = ''
+        checked.value = false
+
+        // }
+
+    } else {
+        showToast('信息没有填完整')
+    }
 
 }
 
@@ -159,51 +260,6 @@ const onSave = (val: any) => {
         message: '添加成功',
         icon: 'success',
     });
-}
-const onDelete = (val: any) => {
-    console.log(val)
-    showToast('delete')
-};
-
-const onEdit = (item: any, index: any) => showToast('编辑地址:' + index);
-const goLocation = () => {
-    showToast('跳到定位')
-
-}
-
-//方法二的方法，没有用
-const Changeadd = (value: any) => {
-    console.log(value)
-    changevalue.value = value
-}
-const addSave = () => {
-    if (name.value && tel.value && address.value && diqu.value != '') {
-        let addressList = localStorage.addressList || `[]`;
-        addressList = JSON.parse(addressList);
-
-        addressList.push({
-            id: new Date().getTime(),
-            name: name.value,
-            tel: tel.value,
-            address: address.value,
-            diqu: '1111',
-            isDefault: changevalue.value
-        });
-        list.value = addressList
-        localStorage.addressList = JSON.stringify(addressList);
-        // console.log(val)
-        console.log(addressList)
-        showToast({
-            message: '添加成功',
-            icon: 'success',
-        });
-        name.value = ''
-        tel.value = ''
-        address.value = ''
-    } else {
-        showToast('信息没有填完整')
-    }
-
 }
 
 </script>
